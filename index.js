1fbaaf4de2898ffc7d9b7a81eb3158cde2a8a720
Justin Phillips <Jukphillips1990@gmail.com>
2021-08-24 09:53:24 -0600

    crated file structure

---
+++ index.js
@@ -0,0 +1,12 @@
+// Including inquirer npm
+const inquirer = require('inquirer')
+// Including file system 
+const fs = require('fs')
+// Including Employee.js into the file
+const Employee = require('./classes/employee')
+// Including Engineer.js into the file
+const Engineer = require('./classes/engineer')
+// Including Intern.js into the file
+const Intern = require('./classes/intern')
+// Including Manager into the file
+const Manager = require('./classes/manager')
---
+++ tests/Employee.test.js
@@ -0,0 +1,1 @@
+const employee = require("../classes/employee")
