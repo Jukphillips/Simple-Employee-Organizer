76e0c9be4ca5387c69d2f659f20e266fd70d3682
Justin Phillips <Jukphillips1990@gmail.com>
2021-08-24 22:59:07 -0600

    Created Engineer test and passed all tests

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,1 +1,31 @@
-const engineer = require('../classes/engineer')
+const Engineer = require('../classes/engineer')
+
+describe("Engineer", () => {
+
+    it("Can it set github account via constructor", () => {
+        const github = "juk"
+
+        const obj = new Engineer("justin", 1, "j@gmail.com", github)
+
+        expect(obj.github).toEqual(github)
+    })
+
+    it("getRole() should return Engineer", () => {
+
+        const role = "Engineer"
+
+        const obj = new Engineer("justin", 1, "j@gmail.com", "juk")
+
+        expect(obj.getRole()).toEqual(role)
+    })
+
+    it("Can get Github Username via getgithub()", () => {
+         const github = "juk"
+
+        const obj = new Engineer("justin", 1, "j@gmail.com", github)
+
+        expect(obj.getGithub()).toEqual(github)
+    })
+
+    })
+    
