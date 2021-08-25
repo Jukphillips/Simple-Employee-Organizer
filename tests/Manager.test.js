aaacf8a41b84ca659bb0087ec3e5e4061272c2f9
Justin Phillips <Jukphillips1990@gmail.com>
2021-08-24 22:25:01 -0600

    Fixed bug in manager.js and created manager test that passes all tests

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,8 +1,36 @@
-const Manager = require('../classes/employee.js')
+const Manager = require('../classes/manager')
+
+describe("Manager", () => {
+
+    it("Can it set office number via constructor argument", () => {
+        const number = 203
+
+        const obj = new Manager("justin", 1, "j@gmail.com", number)
+
+        expect(obj.officeNumber).toEqual(number)
+
+    })
+
+    it("getRole should return 'Manager'", () => {
+        const role = "Manager"
+
+        const obj = new Manager("justin", 1, "j@gmail.com", 203)
+
+        expect(obj.getRole()).toEqual(role)
+
+    })
+
+    it("Can it get office number via getOfficeNumber()", () => {
+        const number = 203
+
+        const obj = new Manager("justin", 1, "j@gmail.com", number)
+
+        expect(obj.getOfficeNumber()).toEqual(number)
+
+    })
+
+})
 
 
 
 
-
-
-module.exports = Manager
