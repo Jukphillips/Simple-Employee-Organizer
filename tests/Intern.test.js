e0bef2db18e77b2f5a2facc81dfcb3876f30a5b5
Justin Phillips <Jukphillips1990@gmail.com>
2021-08-24 22:08:43 -0600

    Created intern test that passes all test

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,1 +1,31 @@
-const intern = require('../classes/intern')
+const Intern = require('../classes/intern')
+
+
+
+describe("Intern", () => {
+
+    it("Can it set school via constructor function", () => {
+        const school = "Denver University"
+
+        const obj = new Intern("Justin", 1,"j@gmail.com", school)
+
+        expect(obj.school).toEqual(school)
+    })
+
+    it("getRole() should return 'Intern'", () => {
+        const role = "Intern"
+
+        const obj = new Intern("Justin")
+
+        expect(obj.getRole()).toEqual(role)
+    })
+
+    it("Can it get school via getSchool()", () => {
+        const school = "Denver University"
+
+        const obj = new Intern("Justin", 1,"j@gmail.com", school)
+
+        expect(obj.getSchool()).toEqual(school)
+
+    })
+})
